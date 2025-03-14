d6cc0b1b333b97e4c9ba403cd8ff99dcc41da1be
George <levchenko.george.vata@gmail.com>
2025-03-14 18:06:35 +0100

    Reference colors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  { path: 'login', loadComponent: () => import('./containers/core/login/login.component').then(c => c.LoginComponent) },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   {
     path: '',
@@ -8,6 +9,5 @@ export const routes: Routes = [
     canActivate: [],
     children: [{ path: 'dashboard', loadComponent: () => import('./containers/dashboard/dashboard.component').then(c => c.DashboardComponent) }],
   },
-  { path: 'login', loadComponent: () => import('./containers/core/login/login.component').then(c => c.LoginComponent) },
   { path: '**', loadComponent: () => import('./containers/core/not-found/not-found.component').then(c => c.NotFoundComponent) },
 ];
