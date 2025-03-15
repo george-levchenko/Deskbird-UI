811b2a34de7cad56f46feef837f52ccdd67cf6f5
George <levchenko.george.vata@gmail.com>
2025-03-15 19:14:26 +0100

    Dashboard (fixes + popup)

--- src/app/containers/dashboard/dashboard.component.ts
+++ src/app/containers/dashboard/dashboard.component.ts
@@ -1,23 +1,29 @@
-import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { User } from '../../models/user.model';
 import { Card } from 'primeng/card';
 import { TableModule } from 'primeng/table';
 import { ButtonDirective } from 'primeng/button';
 import { Tooltip } from 'primeng/tooltip';
 import { EllipsisPipe } from '../../utils/pipes/ellipsis.pipe';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { ConfirmationService } from 'primeng/api';
+import { DialogModule } from 'primeng/dialog';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [Card, TableModule, ButtonDirective, Tooltip, EllipsisPipe],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [Card, TableModule, Tooltip, EllipsisPipe, ButtonDirective, ConfirmPopupModule, DialogModule],
+  providers: [ConfirmationService],
 })
 export class DashboardComponent implements OnInit {
   readonly isAdmin = signal(false);
   readonly users = signal<User[]>([]);
 
-  ngOnInit() {
+  readonly confirmationService = inject(ConfirmationService);
+
+  ngOnInit(): void {
     //   @Todo Receive isAdmin from Auth
     this.isAdmin.set(true);
 
@@ -29,16 +35,38 @@ export class DashboardComponent implements OnInit {
       { id: '4', username: 'Lalala', password: '1234563', isAdmin: false },
       { id: '3', username: 'VataVata', password: '1234565', isAdmin: false },
       { id: '4', username: 'Lalala', password: '1234563', isAdmin: false },
-      { id: '3', username: 'VataVata', password: '1234565', isAdmin: false },
-      { id: '4', username: 'Lalala', password: '1234563', isAdmin: false },
-      { id: '3', username: 'VataVata', password: '1234565', isAdmin: false },
-      { id: '4', username: 'Lalala', password: '1234563', isAdmin: false },
       { id: '5', username: 'Omgmgmgmg', password: '1234563', isAdmin: false },
     ]);
   }
 
-  editUser(user: User) {
+  createUser(): void {
+    // @ToDo open dialog
+  }
+
+  editUser(user: User): void {
     console.log(user);
     // @ToDo open dialog
+  }
+
+  deleteUser(event: Event, user: User): void {
+    // Known issue with miss-alignment of popup on smaller screens
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Do you want to delete this user?',
+      icon: 'pi pi-info-circle',
+      rejectButtonProps: {
+        label: 'Cancel',
+        severity: 'secondary',
+        outlined: true,
+      },
+      acceptButtonProps: {
+        label: 'Delete',
+        severity: 'danger',
+      },
+      accept: () => {
+        console.log(user);
+        //ToDo delete user action
+      },
+    });
   }
 }
