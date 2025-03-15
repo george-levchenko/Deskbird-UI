a6930563a05d15baff7d9e00d8c65fa796890715
George <levchenko.george.vata@gmail.com>
2025-03-15 12:12:37 +0100

    Login Page (wrong password)

--- src/app/containers/core/login/login.component.ts
+++ src/app/containers/core/login/login.component.ts
@@ -6,19 +6,28 @@ import { InputText } from 'primeng/inputtext';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Password } from 'primeng/password';
 import { Button } from 'primeng/button';
+import { UntilDestroy } from '@ngneat/until-destroy';
+import { Store } from '@ngrx/store';
+import { Message } from 'primeng/message';
 
 @Component({
   selector: 'app-login',
-  imports: [NgOptimizedImage, Card, FloatLabel, InputText, ReactiveFormsModule, Password, Button, NgTemplateOutlet],
+  imports: [NgOptimizedImage, Card, FloatLabel, InputText, ReactiveFormsModule, Password, Button, NgTemplateOutlet, Message],
   templateUrl: './login.component.html',
   styleUrl: './login.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
+@UntilDestroy()
 export class LoginComponent implements OnInit {
   form!: FormGroup;
+  minLength = 6;
+  maxLengthUsername = 60;
+  maxLengthPassword = 100;
 
   readonly loading = signal(false);
+
   readonly fb = inject(FormBuilder);
+  readonly store = inject(Store);
 
   get usernameField() {
     return this.form.get('username');
@@ -30,12 +39,15 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      username: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(60)]],
-      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(100)]],
+      username: ['', [Validators.required, Validators.minLength(this.minLength), Validators.maxLength(this.maxLengthUsername)]],
+      password: ['', [Validators.required, Validators.minLength(this.minLength), Validators.maxLength(this.maxLengthPassword)]],
     });
+
+    // @Todo Implement Select from Auth Error and this.form.setErrors({unauthenticated: true})
   }
 
   onSubmit(): void {
     this.loading.set(true);
+    // @Todo Implement Action Login
   }
 }
