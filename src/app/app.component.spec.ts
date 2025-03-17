395f3453439790ccbafef25dd33af445c855b7dd
George <levchenko.george.vata@gmail.com>
2025-03-17 12:56:31 +0100

    tests fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { AuthService } from './utils/services/auth.service';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [AuthService, JwtHelperService],
     }).compileComponents();
   });
 
